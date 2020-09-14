de04485cb72a97c03b270e83745d4e8cf4604de3
Saif Al-Din Ali <saifaldindeveloper@gmail.com>
2020-09-14 19:20:04 -0400

    Added basic text editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import { createEditor } from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
 
-function App() {
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Slate editor={editor} value={value} onChange={(newValue: any) => setValue(newValue)}>
+      <Editable
+        // Define a new handler which prints the key that was pressed.
+        onKeyDown={event => {
+          if (event.key === '&') {
+            // Prevent the ampersand character from being inserted.
+            event.preventDefault()
+            // Execute the `insertText` method when the event occurs.
+            editor.insertText("and")
+          }
+        }}
+      />
+    </Slate>
+  )
 }
-
 export default App;
